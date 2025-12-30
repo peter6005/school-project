7ebc02fb87ae3be827befd367706df268101e9b7
Peter <email@example.com>
2025-12-30 09:30:57 +0100

    enable usb debug

--- mikrobi-cansat.c
+++ mikrobi-cansat.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
 int main()
 {
     stdio_init_all();
+
+    // Wait for USB connection
+    while (stdio_usb_connected() == false) sleep_ms(100);
 
     while (true) {
         printf("Hello, world!\n");
