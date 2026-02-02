0af0e8b3fe7d2ebcb956e5acf31d64bdfc9c2e04
peter6005 <144058743+peter6005@users.noreply.github.com>
2026-02-02 20:58:00 +0100

    Update sd_camera.ino

--- v2/sd_camera/sd_camera.ino
+++ v2/sd_camera/sd_camera.ino
@@ -4,11 +4,7 @@
 
 using namespace eloq;
 
-#define TRIGGER_PIN 47
-#define LED_PIN     2
-
-static uint32_t imgCounter = 0;
-static int lastState = HIGH;
+#define LED_PIN 2
 
 void setup() {
     // kill radios asap
@@ -16,46 +12,27 @@ void setup() {
     WiFi.disconnect(true);
     btStop();
 
-    pinMode(TRIGGER_PIN, INPUT_PULLUP);
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, LOW);
-
     camera.pinout.esp32_s3_cam();
     camera.resolution.uxga();
     camera.quality.best();
-
-    while (!camera.begin().isOk()) { }
 
     sdmmc.pinout.clk(39);
     sdmmc.pinout.cmd(38);
     sdmmc.pinout.d0(40);
 
-    while (!sdmmc.begin().isOk()) { }
+    while (!camera.begin().isOk()) {}
+    while (!sdmmc.begin().isOk()) {}
 }
 
 void loop() {
-    int s = digitalRead(TRIGGER_PIN);
-
-    if (s == LOW && lastState == HIGH) {
-        captureFast();
-    }
-
-    lastState = s;
-}
-
-inline void captureFast() {
-    digitalWrite(LED_PIN, HIGH);
-
     if (!camera.capture().isOk()) {
-        digitalWrite(LED_PIN, LOW);
         return;
     }
 
-    char fname[32];
-    imgCounter++;
-    snprintf(fname, sizeof(fname), "/i%lu.jpg", imgCounter);
-
-    sdmmc.save(camera.frame).to(fname, "jpg");
-
+    digitalWrite(LED_PIN, HIGH);
+    sdmmc.save(camera.frame).to("", "jpg");
     digitalWrite(LED_PIN, LOW);
+    delay(100);
 }
